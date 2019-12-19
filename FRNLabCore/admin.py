7d9b4407b995e33e76702d008993ee349c2c2387
Verkeys Francis <36914841+verfran@users.noreply.github.com>
2019-12-19 22:22:26 +0530

    create person model

--- FRNLabCore/admin.py
+++ FRNLabCore/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+from import_export.admin import ImportExportModelAdmin
+
 
 # Register your models here.
+@admin.register(Person)
+class PersonAdmin(ImportExportModelAdmin):
+	list_display = ('firstName', 'secondName', 'gender', 'dateOfBirth', 'notes',
+		'phoneNumber', 'email')
--- FRNLabCore/models.py
+++ FRNLabCore/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+	firstName = models.CharField(max_length=30)
+	secondName = models.CharField(max_length=30, null = True, blank=True)
+
+	GENDER_CHOICE = (
+		('M', 'Male'),
+		('F', 'Female'),
+		)
+	gender = models.CharField(max_length=1,choices = GENDER_CHOICE, default = 'M')
+	dateOfBirth = models.DateField(null=True, blank=True)
+	notes = models.TextField(blank=True, null = True)
+	phoneNumber = models.CharField(max_length=15,blank=True, null=True)
+	email = models.EmailField(max_length=50,blank=True, null=True)
+
+	def __str__(self):
+		name = self.firstName
+		if self.secondName:
+			name = name + " " + self.secondName
+		return name
+
+
+ 
